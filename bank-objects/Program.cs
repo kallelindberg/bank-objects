7a18ee4b02dfd412063886835082275d52e9e6b1
Kalle Lindberg <kalle.lindberg@student.saimia.fi>
2017-03-01 13:43:43 +0200

    - minor tweaks

--- bank-objects/Program.cs
+++ bank-objects/Program.cs
@@ -14,30 +14,39 @@ namespace bank_objects
             Random rnd = new Random();
             var date = DateTime.Now;
 
-            var bank = new Bank("OP"); //Create Bank object
-            var cst1 = new Customer("Asiakas", "Yksi"); //Create customers
+            //Create Bank object
+            var bank = new Bank("OP"); 
+            
+            //Create customers
+            var cst1 = new Customer("Asiakas", "Yksi"); 
             var cst2 = new Customer("Asiakas", "Kaksi");
             var cst3 = new Customer("Asiakas", "Kolme");
-            
-            var acc1 = new Account(cst1, bank); //Create bank account
+
+            //Create bank account
+            var acc1 = new Account(cst1, bank); 
             var acc2 = new Account(cst2, bank);
             var acc3 = new Account(cst3, bank);
-            
 
-            var entry1 = new Entry(acc1, 1000, DateTime.Now);//set initial balance via entries
-            var entry2 = new Entry(acc2, 2000, DateTime.Now);
-            var entry3 = new Entry(acc3, 3000, DateTime.Now);
-            
-            for (int c = 0; c < 200; c++) //generate random bank entries
+            //set initial balance fo the accounts
+            for (int c=0; c < 3; c++) 
+            {
+                var entry = new Entry(bank.accounts[c], 3000, DateTime.Now);
+            }
+
+            //generate random bank entries    
+            for (int c = 0; c < 200; c++) 
             {
                 var entry = new Entry(bank.accounts[rnd.Next(0, 3)], rnd.Next(-9000, 9000), date);
                 date = date.AddHours(rnd.Next(0, 9));
             }
-            Console.WriteLine(cst1.ToString(bank)); //print balance for every account
+
+            //print balance for every account
+            Console.WriteLine(cst1.ToString(bank)); 
             Console.WriteLine(cst2.ToString(bank));
             Console.WriteLine(cst3.ToString(bank));
-            
-            Console.WriteLine("\n" + bank.GetEntriesLINQ(acc2,DateTime.Now.AddDays(2), DateTime.Now.AddDays(8))); //query transactions from Account1
+
+            //query transactions from Account1
+            Console.WriteLine("\n" + bank.GetEntriesLINQ(acc2,DateTime.Now.AddDays(2), DateTime.Now.AddDays(8))); 
 
             Console.ReadKey();
         }
